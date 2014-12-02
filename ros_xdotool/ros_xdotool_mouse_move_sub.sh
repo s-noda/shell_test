731261a014e0b8c8a97ea36db1ac6316729544d6
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-12-03 04:11:21 +0900

    initial commit of ros_xdotool_mouse_move_sub.sh

---
+++ ros_xdotool/ros_xdotool_mouse_move_sub.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+MESSAGE="standby ros_xdotool_mouse_move_sub.sh";
+## rostopic pub -1 /ros_xdotool/mouse_move/vector std_msgs/Float32MultiArray "{data: [0,0]}";
+echo $MESSAGE;
+
+rostopic echo "/ros_xdotool/mouse_move/vector" | while read line ;
+do
+    if [ "`echo $line | grep data\:`" ];
+    then
+	BUF=`echo $line | sed "s/^\ *data\:\ \(.\+\)\ *$/\\1/g"`;
+	echo  "receive message: $BUF";
+	CNT=0;
+	CMD="xdotool mousemove ";
+	for val in `echo "$BUF" | sed "s/\[\|\]//g" | sed "s/[\,\|\ ]\+/\ /g"`;
+	do
+	    CMD="$CMD $val";
+	    CNT=`expr $CNT + 1`;
+	done;
+	if [ "$CNT" -eq "2" ];
+	then
+	    eval "$CMD";
+	else
+	    echo -e " \e[1;31minvalid vector command length $CNT\e[m";
+	fi
+    fi;
+done
